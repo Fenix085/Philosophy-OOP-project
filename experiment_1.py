a8da5bb97458591ec1d5633fad1a06bd24e632d2
Volodymyr <79228320+Fenix085@users.noreply.github.com>
2025-05-16 18:27:14 +0200

    add Problem class for "levels" and ProblemResult class to hopefully make something out of it

--- experiment_1.py
+++ experiment_1.py
@@ -1,10 +1,12 @@
 #Some name
 #For now, Tram dilemma
 
+import threading
+
 class People():
     def __init__(self):
         pass
-    def death(self):
+    def isDead(self):
         pass
 
 class Volunteer(People):
@@ -16,3 +18,29 @@ class Passenger(People):
 class Tram():
     def __init__(self):
         pass
+
+class Problem():
+    def __init__(self, name, tr_left, tr_right, description=None, user_input=["pull", "not pull"]):
+        self.name = name
+        self.tr_left = tr_left
+        self.tr_right = tr_right
+        self.description = description
+
+    def play(self):
+        print(self.description)
+        threading.Timer(10, timer)        
+        choice = input("What will you do? ").strip().lower()
+        # if choice in self.user_input:
+        #     match choice:
+        #         case "pull":
+        #             print("You desided to pull the lever")
+        #             print("The tram has changed its route")
+        #             print()
+
+class ProblemResult(Problem):
+    def __init__(self):
+        pass
+
+
+def timer():
+    pass
